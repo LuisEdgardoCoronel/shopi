593129ef507d58dd2305658cac62226fdeecbbd4
Luis Edgardo Coronel <lecoronel02@gmail.com>
2023-08-20 23:22:58 -0300

    agregue navbar

--- src/pages/app/App.tsx
+++ src/pages/app/App.tsx
@@ -7,6 +7,7 @@ import MyOrders from '../MyOrders';
 import NotFound from '../NotFound';
 import SignIn from '../SignIn';
 import './App.css'
+import Navbar from '../../components/Navbar';
 
 
 const AppRoutes: React.FC = ()=>{
@@ -27,6 +28,7 @@ function App():JSX.Element {
     <>
       <BrowserRouter>
         <AppRoutes/>
+        <Navbar/>
       </BrowserRouter>
     </>
   )
