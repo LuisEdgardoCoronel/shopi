ab1057a5f9590e9c1903268cb52b256cc1b9d8c8
Luis Edgardo Coronel <lecoronel02@gmail.com>
2023-08-21 20:35:06 -0300

    creando home

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,5 +1,34 @@
-export default function home():JSX.Element {
+import { useState, useEffect } from "react"
+import Layout from "../../components/layout"
+import Card from "../../components/Card"
+
+export default function Home():JSX.Element {
+
+  const [items, setItems] = useState<string[]>([])
+
+  useEffect(()=>{
+    fetch('https://api.escuelajs.co/api/v1/products')
+    .then(response => response.json())
+    .then(data => setItems(data))
+    
+  },[])
+
   return (
-    <div>index</div>
+    <Layout>
+      <div className="grid gap-4 grid-cols-4 w-full max-w-screen-lg">
+        {
+          items?.map((item) =>{
+            return <Card 
+                    key={item.id} 
+                    price={item.price}
+                    title={item.title}
+                    image={item.images[0]}
+                    category={item.category.name}          
+                  />
+          })
+        }
+      </div>
+      
+    </Layout>
   )
 }
