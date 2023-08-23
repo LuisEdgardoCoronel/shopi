e9103a1559315e875631732700845ff93e66f854
Luis Edgardo Coronel <lecoronel02@gmail.com>
2023-08-22 21:09:01 -0300

    agregando funciones y estados de product detail

--- src/context/index.tsx
+++ src/context/index.tsx
@@ -1,17 +1,44 @@
 import React, {createContext, useState} from 'react'
-import { PropsType, ShoppingCartContextType } from '../interface'
+import { Cards, PropsType, ShoppingCartContextType } from '../interface'
 
 export const ShoppingCartContext = createContext<ShoppingCartContextType|undefined>(undefined)
 
 export const ShoppingCartProvider:React.FC<PropsType> = ({children})=>{
-
+  //incrementar shopping cart
   const [count, setCount] = useState<number>(0)
+
+  // product detail. open/close
+  const [isProductDetailOpen, setIsProductDetailOpen] = useState<boolean>(false)
+  const toggleProductDetail:()=>void = () => setIsProductDetailOpen(!isProductDetailOpen);
+
+
+  //rellenar product detail
+  const [productToShow, setProductToShow] = useState<Partial<Cards>>({
+    key:0,
+    price:0,
+    title:'',
+    image:'',
+    category:'',
+    description: '',
+  })
+
+  const showProduct  = (price:number, title:string, image:string, category:string,description:string) =>{
+    toggleProductDetail()
+    setProductToShow({price, title, image, category,description})
+  }
+
 
 
   return(
     <ShoppingCartContext.Provider value={{
       count,
       setCount,
+      toggleProductDetail,
+      isProductDetailOpen,
+      setIsProductDetailOpen,
+      productToShow,
+      setProductToShow,
+      showProduct,
     }}>
       {children}
     </ShoppingCartContext.Provider>
