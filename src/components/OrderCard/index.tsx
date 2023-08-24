5847774ad08dfa6561c25d6ecdb06dc5202fd46b
Luis Edgardo Coronel <lecoronel02@gmail.com>
2023-08-23 21:33:13 -0300

    ajustes de estilos y funcion

--- src/components/OrderCard/index.tsx
+++ src/components/OrderCard/index.tsx
@@ -1,20 +1,27 @@
 import { XCircleIcon } from "@heroicons/react/24/solid"
-import { Cards } from "../../interface"
+import { Cards, ShoppingCartContextType } from "../../interface"
+import { useContext } from "react"
+import { ShoppingCartContext } from "../../context"
 
 
-export const OrderCard:React.FC<Partial<Cards>>  = ({price, title, image}) => {
+export const OrderCard:React.FC<Partial<Cards>>  = ({id,price, title, image}) => {
+
+  const context = useContext(ShoppingCartContext ) as ShoppingCartContextType
+
+  
   return (
-    <div className="flex justify-between items-center mb-3">
+    <div className="flex justify-between items-center px-1 mb-3 w-full box-border">
       <div className="flex items-center gap-2">
         <figure className="w-20 h-20">
           <img className="w-full h-full rounded-lg object-cover" src={image} alt={title} />
         </figure>
-        <p className="text-sm font-light w-40">{title}</p>
+        <p className="text-sm font-light w-36">{title}</p>
       </div>
-      <div className="flex items-center gap-2">
-        <p className="text-lg font-medium">{price}</p>
+      <div className="flex items-center gap-2 ">
+        <p className="text-lg font-medium">$ {price}</p>
         <XCircleIcon 
           className=" w-7 cursor-pointer text-red-600 "
+          onClick={()=>context.handleDelete(id)}
           />
       </div>
     </div>
