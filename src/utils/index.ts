4c000a32724d9212637dcd9f657d32026498b5f2
Luis Edgardo Coronel <lecoronel02@gmail.com>
2023-08-25 22:37:34 -0300

    funcion generadora de productos

---
+++ src/utils/index.ts
@@ -0,0 +1,55 @@
+import { faker } from '@faker-js/faker';
+import { Product } from '../interface';
+
+
+
+
+
+
+function getRandomStringFromArray(strings: string[]): string | undefined {
+  if (strings.length === 0) {
+    return undefined; // Retorna undefined si el array está vacío
+  }
+  const randomIndex = Math.floor(Math.random() * strings.length);
+  return strings[randomIndex];
+}
+
+
+const categories = ['Clothes', 'Electronic', 'Furnitures', 'Toys', 'Others'];
+
+
+
+
+
+
+
+
+// Generar datos falsos de productos
+const generateFakeProducts = (count: number): Product[] => {
+  const fakeProducts: Product[] = [];
+
+  for (let i = 1; i <= count; i++) {
+    const product: Product = {
+      id: i,
+      title: faker.commerce.productName(),
+      price: faker.number.float({ min: 10, max: 100, precision: 0.01 }),
+      description: faker.lorem.paragraph(),
+      category: {
+        id: faker.number.int({ min: 1, max: 10 }),
+        name: getRandomStringFromArray(categories),
+        image: faker.image.url(),
+      },
+      images: [
+        faker.image.url(),
+        faker.image.url(),
+        faker.image.url(),
+      ],
+    };
+
+    fakeProducts.push(product);
+  }
+
+  return fakeProducts;
+};
+
+export default generateFakeProducts;
