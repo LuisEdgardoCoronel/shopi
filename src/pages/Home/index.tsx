a7d273757c5bfe84acb91f2bc9c329c9f2a62d81
Luis Edgardo Coronel <lecoronel02@gmail.com>
2023-08-25 22:40:20 -0300

    funcion de faker

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,24 +1,31 @@
-import { useState, useEffect } from "react";
+// import { useState, useEffect } from "react";
 import Layout from "../../components/layout";
 import Card from "../../components/Card";
 import { ProductDetail } from "../../components/ProductDetail";
 
+
+import generateFakeProducts from '../../utils/index.ts'
+
+const fakeProducts = generateFakeProducts(50);
+
+
+
 export default function Home():JSX.Element {
 
-  const [items, setItems] = useState<string[]>([])
+  // const [items, setItems] = useState<string[]>([])
 
-  useEffect(()=>{
-    fetch('https://api.escuelajs.co/api/v1/products')
-    .then(response => response.json())
-    .then(data => setItems(data))
+  // useEffect(()=>{
+  //   fetch('https://api.escuelajs.co/api/v1/products')
+  //   .then(response => response.json())
+  //   .then(data => setItems(data))
     
-  },[])
+  // },[])
 
   return (
     <Layout>
       <div className="grid gap-4 grid-cols-4 w-full max-w-screen-lg">
         {
-          items?.map((item) =>{
+          fakeProducts?.map((item) =>{
             return <Card 
                     key={item.id} 
                     id={item.id}
