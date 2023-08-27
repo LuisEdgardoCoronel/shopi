0f2404fd48216be9e10cc8734989d08d93f408f2
Luis Edgardo Coronel <lecoronel02@gmail.com>
2023-08-26 23:32:56 -0300

    quite funciones y agregue funcion render

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -2,40 +2,71 @@
 import Layout from "../../components/layout";
 import Card from "../../components/Card";
 import { ProductDetail } from "../../components/ProductDetail";
+import { useContext } from "react";
+import { ShoppingCartContext } from "../../context/index.tsx";
+import { ShoppingCartContextType } from "../../interface/index.tsx";
 
 
-import generateFakeProducts from '../../utils/index.ts'
-
-const fakeProducts = generateFakeProducts(50);
 
 
 
 export default function Home():JSX.Element {
 
-  // const [items, setItems] = useState<string[]>([])
+  const context = useContext(ShoppingCartContext ) as ShoppingCartContextType
 
-  // useEffect(()=>{
-  //   fetch('https://api.escuelajs.co/api/v1/products')
-  //   .then(response => response.json())
-  //   .then(data => setItems(data))
-    
-  // },[])
+  // searchItemsCategory
+  
+
+
+  const renderView = ()=>{
+      if (context.filteredItems?.length > 0) {
+        return(
+          context.filteredItems?.map((item) => (<Card 
+            key={item.id} 
+            id={item.id}
+            price={item.price}
+            title={item.title}
+            image={item.images[0]}
+            category={item.category.name}     
+            description={item.description}     
+          />)
+          )
+        )
+      } else {
+        return(
+          <div>No se encontraron coincidencias</div>
+        )
+      }
+    } 
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
 
   return (
     <Layout>
-      <div className="grid gap-4 grid-cols-4 w-full max-w-screen-lg">
+      <div className=" w-full relative mb-4">
+        <h1 className=" text-center text-lg font-semibold">Exclusive Products</h1>
+      </div>
+
+      <input 
+      type="text" 
+      placeholder="Buscar productos" 
+      className=" p-4 mb-4 rounded-lg text-center border focus:outline-none w-80 "
+      onChange={(event)=>context.setSearchTitleItems(event.target.value)}/>
+      <div className="grid gap-4 grid-cols-4 w-full max-w-screen-lg max-lg:grid-cols-3 place-items-center max-sm:grid-cols-2">
         {
-          fakeProducts?.map((item) =>{
-            return <Card 
-                    key={item.id} 
-                    id={item.id}
-                    price={item.price}
-                    title={item.title}
-                    image={item.images[0]}
-                    category={item.category.name}     
-                    description={item.description}     
-                  />
-          })
+          renderView()
         }
       </div>
       <ProductDetail/>
