51dfb058d331c8c55cd29fe6396421f3929524d5
Luis Edgardo Coronel <lecoronel02@gmail.com>
2023-08-26 23:36:07 -0300

    funciones de buscador, mobilemenu

--- src/context/index.tsx
+++ src/context/index.tsx
@@ -1,5 +1,6 @@
-import React, {createContext, useState} from 'react'
-import { Cards, Order, PropsType, ShoppingCartContextType } from '../interface'
+import React, {createContext, useEffect, useState} from 'react'
+import { Cards, Order, Product, PropsType, ShoppingCartContextType } from '../interface'
+import generateFakeProducts from '../utils'
 
 export const ShoppingCartContext = createContext<ShoppingCartContextType|undefined>(undefined)
 
@@ -39,11 +40,9 @@ export const ShoppingCartProvider:React.FC<PropsType> = ({children})=>{
 
   //shopping cart . add products
   const [cartProduct, setCartProduct] = useState<Partial<Cards>[]>([])
-  const [count, setCount] = useState<number>(0)//TODO:analizar si es necesario
-
+  
   const addProductsToCart = (event: React.MouseEvent<HTMLDivElement>,id:number, price:number, title:string, image:string, category:string,description:string) =>{
     event.stopPropagation()
-    setCount(count + 1)
     OpenCheckoutSideMenu()
     setCartProduct([...cartProduct, {id, price, title, image, category,description}])
   }
@@ -53,7 +52,7 @@ export const ShoppingCartProvider:React.FC<PropsType> = ({children})=>{
     setCartProduct(filteredProducts)
   }
 
-  const totalPrice = parseInt(cartProduct.reduce((x,product)=> x + product.price,0).toFixed(2))
+  const totalPrice = parseInt(cartProduct.reduce((x,product)=> product.price != undefined? x + product.price:x,0).toFixed(2))
   const totalCart = cartProduct.length
 
   const [order, setOrder]=useState<Partial<Order>[]>([])
@@ -62,13 +61,13 @@ export const ShoppingCartProvider:React.FC<PropsType> = ({children})=>{
     const orderToAdd ={
       date:'01.02.23',
       products:cartProduct,
-      totalProducts:count,
+      totalProducts:totalCart,
       totalPriceCart: totalPrice
     }
     setOrder([...order, orderToAdd])
-    setCount(0)
     setCartProduct([]);
     CloseCheckoutSideMenu()
+    setSearchTitleItems(null)
   }
 
 
@@ -77,7 +76,7 @@ export const ShoppingCartProvider:React.FC<PropsType> = ({children})=>{
 
 
 
-    // product detail. open/close
+    // CheckoutSideMenu. open/close
     const [isCheckoutSideMenuOpen, setIsCheckoutSideMenuOpen] = useState<boolean>(false)
     const OpenCheckoutSideMenu:()=>void = () => setIsCheckoutSideMenuOpen(true);
     const CloseCheckoutSideMenu:()=>void = () => setIsCheckoutSideMenuOpen(false);
@@ -89,10 +88,72 @@ export const ShoppingCartProvider:React.FC<PropsType> = ({children})=>{
 
 
 
+
+
+
+    //get products . get products by title
+    const fakeProducts = generateFakeProducts(50);
+    const [items, setItems] = useState<Product[]>(fakeProducts)
+    // useEffect(()=>{
+    //   fetch('https://api.escuelajs.co/api/v1/products')
+    //   .then(response => response.json())
+    //   .then(data => setItems(data))
+      
+    // },[])
+    const [filteredItems, setFilteredItems] = useState<Product[]>(fakeProducts)
+    const [searchTitleItems, setSearchTitleItems] = useState<Product[]|string|null>(null)
+    const filteredItemsByTitle: (items: Product[], searchTitleItems: string) => Product[] = ( items:Product[], searchTitleItems:string )=>{
+      return items?.filter(item => item.title.toLowerCase().includes(searchTitleItems.toLowerCase()))
+    }
+    // useEffect(()=>{
+    //   if (searchTitleItems)  setFilteredItems(filteredItemsByTitle(items,searchTitleItems))
+      
+    // },[items,searchTitleItems])
+
+    const [searchItemsCategory, setSearchItemsCategory] = useState<Product[]|string|null>(null)
+    
+    const filteredItemsByCategory: (items: Product[], searchCategoryItems: string) => Product[] = ( items:Product[], searchCategoryItems:string )=>{
+      return items?.filter(item => item.category.name.toLowerCase().includes(searchCategoryItems.toLowerCase()))
+    }
+
+    const filterBy=(searchType:string|null,items:Product[], searchTitleItems:string, searchItemsCategory:string)=>{
+      if (searchType === 'BY_TITLE')return filteredItemsByTitle(items, searchTitleItems)
+      if (searchType === 'BY_CATEGORY') return filteredItemsByCategory(items, searchItemsCategory)
+      if (searchType === 'BY_TITLE_AND_CATEGORY') return filteredItemsByCategory(items, searchItemsCategory).filter(item => item.title.toLowerCase().includes(searchTitleItems.toLowerCase()))
+      if (!searchType) return items
+    }
+
+    useEffect(()=>{
+      if (searchTitleItems && searchItemsCategory)  setFilteredItems(filterBy('BY_TITLE_AND_CATEGORY',items,searchTitleItems,searchItemsCategory))
+      if (searchTitleItems && !searchItemsCategory)  setFilteredItems(filterBy('BY_TITLE',items,searchTitleItems,searchItemsCategory))
+      if (searchItemsCategory && !searchTitleItems )  setFilteredItems(filterBy('BY_CATEGORY',items,searchTitleItems,searchItemsCategory))
+      if (!searchItemsCategory && !searchTitleItems )  setFilteredItems(filterBy(null,items,searchTitleItems,searchItemsCategory))
+      // return()=>setSearchTitleItems(null)
+    },[items,searchTitleItems,searchItemsCategory])
+
+
+
+    
+
+
+
+    //mobile menu
+    const[openMobileMenu, setOpenMobileMenu] = useState<boolean>(false)
+
+
+
+
+
+
+
+
+
+
+
+
+
   return(
     <ShoppingCartContext.Provider value={{
-      count,
-      setCount,
       toggleProductDetail,
       isProductDetailOpen,
       setIsProductDetailOpen,
@@ -111,7 +172,17 @@ export const ShoppingCartProvider:React.FC<PropsType> = ({children})=>{
       totalCart,
       order,
       setOrder,
-      handleCheckout
+      handleCheckout,
+      items,
+      setItems,
+      searchTitleItems, 
+      setSearchTitleItems,
+      filteredItemsByTitle,
+      filteredItems,
+      searchItemsCategory, 
+      setSearchItemsCategory,
+      openMobileMenu,
+      setOpenMobileMenu,
     }}>
       {children}
     </ShoppingCartContext.Provider>
