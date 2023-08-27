d0d59275c765eb86329542e64d8ece449016f4e1
Luis Edgardo Coronel <lecoronel02@gmail.com>
2023-08-26 23:34:30 -0300

    estilos responsive

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -38,10 +38,10 @@ const Card:React.FC<Cards> = ({price, title, image, category, description, id}) 
 
   return (
     <div 
-      className="bg-white cursor-pointer w-56 h-60 rounded-lg  shadow-md" 
+      className="bg-white cursor-pointer w-56 h-60 rounded-lg shadow-md max-sm:w-48 " 
       onClick={()=>context.showProduct(price, title, image, category,description)}
     >
-      <figure className="relative mb-2 w-full h-4/5">
+      <figure className="relative mb-2 w-full h-4/5 max-sm:h-3/4">
         <span className="absolute bottom-0 left-0 bg-white/60 rounded-lg text-black text-sm m-2 px-3 py-0.5">
           {category}
         </span>
