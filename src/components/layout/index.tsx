19fe625ef6441ea350baf54576d0c8807588a9cd
Luis Edgardo Coronel <lecoronel02@gmail.com>
2023-08-26 23:35:03 -0300

    estilos responsive

--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -2,7 +2,7 @@ import { PropsType } from "../../interface";
 
 export default function Layout({ children }: PropsType):JSX.Element {
   return (
-    <div className="flex flex-col mt-20 items-center">
+    <div className="flex flex-col pt-20 items-center  h-screen overflow-y-scroll pb-5">
       {children}
     </div>
   );
