ab73428f34e44108d516f0ed5dce9edb5e29f6a7
Luis Edgardo Coronel <lecoronel02@gmail.com>
2023-08-28 19:14:11 -0300

    agregue componente y texto español

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -56,15 +56,22 @@ export default function Home():JSX.Element {
   return (
     <Layout>
       <div className=" w-full relative mb-4">
-        <h1 className=" text-center text-lg font-semibold">Exclusive Products</h1>
+        <h1 className=" text-center text-lg font-semibold">Productos Exclusivos</h1>
       </div>
+
+      { 
+      context.successfulPurchase ===true? <div className="absolute bg-white p-5 rounded-lg border-2 border-green-500">
+        <p className=" text-green-500 font-semibold">Compra Exitosa!</p>
+      </div>
+      :""
+      }
 
       <input 
       type="text" 
       placeholder="Buscar productos" 
       className=" p-4 mb-4 rounded-lg text-center border focus:outline-none w-80 "
       onChange={(event)=>context.setSearchTitleItems(event.target.value)}/>
-      <div className="grid gap-4 grid-cols-4 w-full max-w-screen-lg max-lg:grid-cols-3 place-items-center max-sm:grid-cols-2">
+      <div className="grid gap-4 grid-cols-4 w-full max-w-screen-lg max-lg:grid-cols-3 max-lg:gap-y-8 place-items-center max-sm:grid-cols-2">
         {
           renderView()
         }
